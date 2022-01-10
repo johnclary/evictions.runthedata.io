98860702a4d91588dc2c1bcf83714c7dbd18ad77
John Clary <john.clary@austintexas.gov>
2022-01-09 21:56:58 -0600

    use env var for gql endpoint

--- components/utils/graphql.js
+++ components/utils/graphql.js
@@ -1,10 +1,10 @@
 import { request } from "graphql-request";
 import useSWR from "swr";
 
-const URL = "http://localhost:8080/v1/graphql";
+const HASURA_GRAPHQL_ENDPOINT = process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ENDPOINT;
 
 const fetcher = async (query, variables) => {
-  return await request(URL, query, variables)
+  return await request(HASURA_GRAPHQL_ENDPOINT, query, variables);
 };
 
 export const useGraphql = ({ query, variables }) => {
